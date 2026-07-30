1e8314398de1416553457f28aea68a6a1b36655b
Ytshak_Raiyner <yr3275744@gmail.com>
2026-07-30 11:04:46 +0300

    first version of interfaces

--- Repositories/Interfaces/IAccommodationRepository.cs
+++ Repositories/Interfaces/IAccommodationRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace BeinHazmanimFinderAPI.Repositories.Interfaces
+﻿using BeinHazmanimFinderAPI.Models;
+
+namespace BeinHazmanimFinderAPI.Repositories.Interfaces;
+
+public interface IAccommodationRepository
 {
-    public interface IAccommodationRepository
-    {
-    }
+    public IEnumerable<Accommodation> GetAll();
+    public Accommodation? GetById(int id);
+    public Accommodation Create();
+    public Accommodation? Update(int id);
+    public bool Delete(int id);
+
 }
--- Repositories/Interfaces/IActivityPlaceRepository.cs
+++ Repositories/Interfaces/IActivityPlaceRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace BeinHazmanimFinderAPI.Repositories.Interfaces
+﻿using BeinHazmanimFinderAPI.Models;
+
+namespace BeinHazmanimFinderAPI.Repositories.Interfaces;
+
+public interface IActivityPlaceRepository
 {
-    public interface IActivityPlaceRepository
-    {
-    }
+    public IEnumerable<ActivityPlace> GetAll();
+    public ActivityPlace? GetById(int id);
+    public ActivityPlace Create();
+    public ActivityPlace? Update(int id);
+    public bool Delete(int id);
+
 }
